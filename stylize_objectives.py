0498a0202ea56137e6397b1045e2748b91a2e5a0
Janet Li <54826451+janet-li@users.noreply.github.com>
2022-02-26 16:21:32 -0500

    Update stylize_objectives.py

--- stylize_objectives.py
+++ stylize_objectives.py
@@ -4,8 +4,8 @@ import torch
 from torch.autograd import Variable
 import torch.nn.functional as F
 
-from .contextual_loss import *
-from . import utils
+import contextual_loss
+import utils
 
 use_random=True
 
